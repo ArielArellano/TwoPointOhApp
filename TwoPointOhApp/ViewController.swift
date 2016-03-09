148fe58b015361afc9eb5efabb35085dbcc1bee6
Ariel Gomez A_ <ariel_gomeza@ariels-macbook-pro.local>
2016-03-09 17:09:24 -0500

    Initial Commit

---
+++ TwoPointOhApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwoPointOhApp
+//
+//  Created by Ariel Gomez A_ on 3/9/16.
+//  Copyright © 2016 Ariel Gomez A_. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
